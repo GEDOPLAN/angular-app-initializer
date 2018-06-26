fc43bfc4e1effb79edd5dc113792522ff9cbc9e8
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2018-06-26 16:43:53 +0200

    demo application

---
+++ README.md
@@ -0,0 +1,21 @@
+# angular app initializer
+
+Demo-Projekt zum Blog Beitrag auf:
+
+`https://javaeeblog.wordpress.com/`
+
+> todo
+
+## Run
+
+Die Anwendung verwendet **Angular CLI**
+
+run:
+
+> npm start
+
+open:
+
+> http://localhost:4200/
+
+_by GEDOPLAN, [Dominik Mathmann](https://github.com/dominikmathmann)_
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import 'rxjs/add/operator/delay';
 
+export function initApplication(http: HttpClient) {
+  return () => {
+    return new Promise((resolve, reject) => {
+      //make some init steps, load data for example
+      http
+        .get('https://jsonplaceholder.typicode.com/users/1')
+        .delay(5000) //wait a little bit, just to enjoy the loading gif
+        .subscribe(r => {
+          //handle response
+          resolve(r);
+        });
+    });
+  };
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [HttpClient],
+      useFactory: initApplication
+    }
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
